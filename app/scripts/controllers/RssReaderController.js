b8c925b33f3cf476efef78592314523212eeed6e
katyf <katy.feng.14@gmail.com>
2015-04-08 17:41:19 -0400

    refactor rss controller to use namespacing, fixes bug in loading user feeds

--- app/scripts/controllers/RssReaderController.js
+++ app/scripts/controllers/RssReaderController.js
@@ -1,18 +1,3 @@
-// 'use strict';
-
-// angular.module('frontendApp').controller('RssReaderController', ['$scope','RssReaderFactory', function ($scope,Feed) {
-//     $scope.loadFeed=function(e){
-//       //debugger;
-//         Feed.parseFeed($scope.feedSrc).then(function(res){
-//             $scope.loadButonText=angular.element(e.target).text();
-//             $scope.feeds=res.data.responseData.feed.entries;
-//             console.log('hi from rss reader controller');
-//         });
-//     };
-// }]);
-
-
-
 'use strict';
 
 angular.module('frontendApp').controller('RssReaderController', rssReaderController);
@@ -23,13 +8,36 @@ angular.module('frontendApp').controller('RssReaderController', rssReaderControl
     var vm = this;
     vm.user = UserFactory.user;
 
-    $scope.loadFeed=function(e){
-        Feed.parseFeed($scope.feedSrc).then(function(res){
-            $scope.loadButonText=angular.element(e.target).text();
-            $scope.feeds=res.data.responseData.feed.entries;
-            console.log('hi from rss reader controller');
+    vm.loadFeed=function(e,feedSrc){
+       // debugger;
+        Feed.parseFeed(feedSrc).then(function(res){
+            $scope.loadButonText = angular.element(e.target).text();
+            $scope.feeds = res.data.responseData.feed.entries;
+            console.log('$scope.feeds: ', res.data.responseData.feed.entries);
         });
     };
 }
 
 
+// 'use strict';
+
+// angular.module('frontendApp').controller('RssReaderController', rssReaderController);
+
+//  rssReaderController.$inject=['$scope','RssReaderFactory', 'UserFactory'];
+
+//  function rssReaderController($scope,Feed, UserFactory) {
+//     var vm = this;
+//     vm.user = UserFactory.user;
+
+//     vm.loadFeed=function(e, feedSrc){
+//         Feed.parseFeed(feedSrc).then(function(res){
+//             vm.loadButonText=angular.element(e.target).text();
+//             vm.feeds=res.data.responseData.feed.entries;
+//             console.log('hi from rss reader controller');
+//         });
+//     };
+
+//      vm.sayhi = function(e){
+//         console.log('heeeey');
+//     };
+// }
