8c3357596f2f120b0bb88179aa36677aa76d63bd
katyf <katy.feng.14@gmail.com>
2015-05-05 22:13:08 -0400

    fix random quote feature

--- app/scripts/factories/QuoteFactory.js
+++ app/scripts/factories/QuoteFactory.js
@@ -4,10 +4,24 @@ angular.module('frontendApp').factory('QuoteFactory', ['$http', 'ServerUrl', fun
 
   var quotes = [];
 
+  var shuffleQuotes = function(quotes) {
+  var length = quotes.length;
+  var t, i;
+  while (length) {
+    i = Math.floor(Math.random() * length--);
+    t = quotes[length];
+    quotes[length] = quotes[i];
+    quotes[i] = t;
+  }
+
+  return quotes;
+};
+
   var getQuotes = function(){
       return $http.get(ServerUrl + '/quotes').success(function(response){
       console.log(response);
       angular.copy(response, quotes);
+      shuffleQuotes(quotes);
     }).error(function(data, status, headers, config){
       console.log('Error:' + data, status, headers, config);
     });
@@ -16,7 +30,8 @@ angular.module('frontendApp').factory('QuoteFactory', ['$http', 'ServerUrl', fun
 
   return {
     getQuotes: getQuotes,
-    quotes: quotes
+    quotes: quotes,
+    shuffleQuotes: shuffleQuotes
   };
 
 }]);
